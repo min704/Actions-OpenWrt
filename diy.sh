75bf3c5d7c8fc080cf44b735cb2097716d452d11
min704 <46183464+min704@users.noreply.github.com>
2020-05-24 19:16:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,9 +8,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+sed -i 's/Openwrt/Kyara/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/hyird/passwall  ./package
-git clone https://github.com/Lienol/openwrt-package package/openwrt-package
 ./scripts/feeds update -a
 ./scripts/feeds install -a
